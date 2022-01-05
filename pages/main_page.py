d9bfc0c207fea1f453a2703a47ac901384d0ead7
Radomur <kkashirsk@yandex.ru>
2022-01-05 19:25:30 +0200

    add test LoginPage

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,5 +1,6 @@
 from .base_page import BasePage
 from .locators import MainPageLocators
+from .locators import LoginPageLocators
 from selenium.webdriver.common.by import By
 
 class MainPage(BasePage):
@@ -8,3 +9,12 @@ class MainPage(BasePage):
        login_link.click()
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+
+    #def should_be_login_url(self):
+    #    assert 'login' in self.browser.current_url, "Login URL form is not presented"
+#
+#    def should_be_login_form(self):
+#        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+
+#    def should_be_register_form(self):
+#        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
