c715e0bac1ce9d9fa0f61da9b9e3c1a4224fa4c7
Radomur <kkashirsk@yandex.ru>
2022-01-05 19:47:45 +0200

    transfer function call test LoginPage

--- pages/main_page.py
+++ pages/main_page.py
@@ -9,12 +9,3 @@ class MainPage(BasePage):
        login_link.click()
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
-
-    #def should_be_login_url(self):
-    #    assert 'login' in self.browser.current_url, "Login URL form is not presented"
-#
-#    def should_be_login_form(self):
-#        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
-
-#    def should_be_register_form(self):
-#        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
