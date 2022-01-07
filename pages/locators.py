c593c67e67523967320ce700c65146fdb28897c8
Radomur <kkashirsk@yandex.ru>
2022-01-07 21:43:20 +0200

    add checks_add_product_to_basket

--- pages/locators.py
+++ pages/locators.py
@@ -7,3 +7,10 @@ class MainPageLocators():
 class LoginPageLocators():
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+
+class ProductPageLocators():
+    BUTTON_BASKET = (By.CSS_SELECTOR, "button.btn-add-to-basket")
+    PRODUCT_MANE = (By.CSS_SELECTOR, ".product_main > h1")
+    PRICE_PRODUCT = (By.CSS_SELECTOR, "p.price_color")
+    PRICE_PRODUCT_TOTAL = (By.CSS_SELECTOR, "div.alertinner  > p > strong")
+    PRODUCT_MANE_MESSAGE = (By.CSS_SELECTOR, ".alertinner > strong")
---
+++ pages/product_page.py
@@ -0,0 +1,27 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+class ProductPage(BasePage):
+
+    def checks_add_product_to_basket(self):
+        self.add_product_to_basket()
+        self.should_product_name_equal_in_message()
+        self.should_product_price_equal_in_basket()
+
+    def add_product_to_basket(self):
+        self.browser.find_element(*ProductPageLocators.BUTTON_BASKET).click()
+        self.solve_quiz_and_get_code()
+
+    def should_product_name_equal_in_message(self):
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_MANE).text
+        product_name_in_message = self.browser.find_element(*ProductPageLocators.PRODUCT_MANE_MESSAGE).text
+        print("product_name: ", product_name)
+        print("product_name_in_message: ", product_name_in_message)
+        assert product_name == product_name_in_message, "Product name and product name in basket didn`t match"
+
+    def should_product_price_equal_in_basket(self):
+        product_price = self.browser.find_element(*ProductPageLocators.PRICE_PRODUCT).text[1:5]
+        product_price_basket_total = self.browser.find_element(*ProductPageLocators.PRICE_PRODUCT_TOTAL).text[1:5]
+        print("product_price: ", product_price)
+        print("product_price_in_message: ", product_price_basket_total)
+        assert product_price == product_price_basket_total, "Product price and price total in basket didn`t match"
