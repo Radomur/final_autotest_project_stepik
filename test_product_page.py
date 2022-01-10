0ad53251d8f91aebb4fa97392cb6250f6c2a668f
Radomur <kkashirsk@yandex.ru>
2022-01-11 01:04:05 +0200

    add mark need_review

--- test_product_page.py
+++ test_product_page.py
@@ -20,6 +20,7 @@ links = ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?pr
 
 @pytest.mark.parametrize("product", links) """
 
+@pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser): #, product: str - если используем parametrize то передаем
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
@@ -49,12 +50,14 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.open()
     page.should_be_login_link()
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = BasketPage(browser, link)
@@ -76,6 +79,7 @@ class TestUserAddToBasketFromProductPage():
         page.register_new_user(email,password)
         page.should_be_authorized_user()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
         page = ProductPage(browser, link)
